cc701a7a2ec52af46e554688850b62fc0c749a32
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-10 08:40:45 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,13 +2,5 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance
   extend MetaDancing
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
 end
