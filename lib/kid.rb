ba58b0fd152328b918b4aa63fbb60ef6fcfa74bc
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-10 09:14:44 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,17 +1,2 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-
-# class Kid
-#   include Dance
-#
-#   attr_accessor :name
-#
-#   def initialize(name)
-#     @name = name
-#   end
-# end
-
-class Kid
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-end
