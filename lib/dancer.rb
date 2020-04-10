4e6f3b6393194b27f59af101b6a98efd4bf05786
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-10 09:13:57 +0000

    Automatically backed up by Learn

---
+++ lib/dancer.rb
@@ -0,0 +1,19 @@
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
+
+# class Dancer
+#   include Dance
+#   extend MetaDancing
+#
+#   attr_accessor :name
+#
+#   def initialize(name)
+#     @name = name
+#   end
+#
+# end
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
